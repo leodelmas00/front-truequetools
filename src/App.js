787ee6fc869e0f33f399ae60fc46b2bffd9f46e3
Lisandro Piva <lisandropiva@gmail.com>
2024-05-01 01:59:08 -0300

    Nuevo login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import logo_1 from './logo_1/logo_1.png'; // Importa la imagen
 
 function App() {
   const [email, setEmail] = useState('');
@@ -12,37 +13,39 @@ function App() {
     // Luego puedes enviar los datos al servidor para autenticación
   };
 
-  // JavaScript para establecer el color de fondo del body
-  document.body.style.backgroundColor = "#f2f0e4";
-
   return (
-    <div align="center">
-      <form onSubmit={handleSubmit}>
-        <div style={{ border: '2px solid black', padding: '5px', margin: '20px', width: '550px', height: '600px' }}>
-        <h1 style={{ fontSize: '5em' }}>Trueque<span style={{ color: '#bf4c41' }}>Tools</span></h1>
-          <h1> Iniciar Sesión </h1>
-          <div align="center">
+    <div style={{ backgroundColor: "#f2f0e4", minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center", position: "relative" }}>
+      
+      
+      
+      <form onSubmit={handleSubmit} style={{ zIndex: 1 }}>
+        <div style={{ border: '2px solid black', padding: '5px', width: '550px', height: '600px' }}>
+          <h1 style={{ fontSize: '5em', textAlign: 'center' }}>Trueque<span style={{ color: '#bf4c41' }}>Tools</span></h1>
+          <h2 style={{ textAlign: 'center' }}>Iniciar Sesión</h2>
+          <div style={{ marginBottom: '20px', textAlign: 'center' }}>
             <input
               type="email"
               placeholder="Correo electrónico"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
-              style={{ width: '400px', height: '40px', fontSize: '1.2em' }} // Tamaño de fuente para las casillas de entrada
+              style={{ width: '400px', height: '40px', fontSize: '1.2em' }}
               required
             />
           </div>
-          <div align="center">
+
+          <div style={{ marginBottom: '20px', textAlign: 'center' }}>
             <input
               type="password"
               placeholder="Contraseña"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              style={{ width: '400px', height: '40px', fontSize: '1.2em' }} // Tamaño de fuente para las casillas de entrada
+              style={{ width: '400px', height: '40px', fontSize: '1.2em' }}
               required
             />
           </div>
-          <div align="center">
-            <button type="submit" style={{ width: '408px', height: '50px', fontSize: '1.2em', backgroundColor: '#f2ada7' }}>Iniciar sesión</button> {/* Modificamos el tamaño de la fuente del botón */}
+
+          <div style={{ textAlign: 'center' }}>
+            <button type="submit" style={{ width: '401px', height: '40px', fontSize: '1.2em', backgroundColor: '#f2ada7', borderColor: '#f2ada7' }}>Iniciar sesión</button>
           </div>
         </div>
       </form>
