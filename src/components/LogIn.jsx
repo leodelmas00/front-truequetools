a2d2cc8168b175f6d751d2b75ae0c9f9434546b1
Lisandro Piva <lisandropiva@gmail.com>
2024-05-01 03:11:16 -0300

    Front con clases qsyo

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'wouter';
+import '../styles/login.css'; // Importa el archivo de estilos CSS
 import FONDO_LOGIN from '../logo_1/FONDO LOGIN.png'; // Importa la imagen
 
 function LogIn() {
@@ -15,49 +16,43 @@ function LogIn() {
     };
 
     return (
-        <div style={{ backgroundColor: "#f2f0e4", minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center", position: "relative" }}>
-
-            <img src={FONDO_LOGIN} alt="Logo" style={{ width: '1920px', height: '1080px', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', zIndex: 1 }} />
-
-            <form onSubmit={handleSubmit} style={{ zIndex: 1 }}>
-                <div style={{ border: '2px solid black', padding: '5px', width: '550px', height: '600px' }}>
-                    <h1 style={{ fontSize: '5em', textAlign: 'center' }}>Trueque<span style={{ color: '#bf4c41' }}>Tools</span></h1>
-                    <h2 style={{ textAlign: 'center' }}>Iniciar Sesión</h2>
-                    <div style={{ marginBottom: '20px', textAlign: 'center' }}>
+        <div className="container">
+            <img src={FONDO_LOGIN} alt="Logo" className="background-img" />
+            <form onSubmit={handleSubmit} className="form-container">
+                <div>
+                    <h1 className="title">Trueque<span className='colorRojo'>Tools</span></h1>
+                    <h2 className="subtitle">Iniciar Sesión</h2>
+                    <div className="input-container">
                         <input
                             type="email"
                             placeholder="Correo electrónico"
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
-                            style={{ width: '400px', height: '40px', fontSize: '1.2em' }}
+                            className="input-field"
                             required
                         />
                     </div>
-                    <div style={{ marginBottom: '20px', textAlign: 'center' }}>
+                    <div className="input-container">
                         <input
                             type="password"
                             placeholder="Contraseña"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
-                            style={{ width: '400px', height: '40px', fontSize: '1.2em' }}
+                            className="input-field"
                             required
                         />
                     </div>
-
-                    <div style={{ textAlign: 'center' }}>
-                        <button type="submit" style={{ width: '401px', height: '40px', fontSize: '1.2em', backgroundColor: '#f2ada7', borderColor: '#f2ada7' }}>Iniciar sesión</button>
+                    <div className="button-container">
+                        <button type="submit" className="signin-link">Iniciar sesión</button>
                     </div>
-                    <div style={{ textAlign: 'center' }}>
+                    <div className="signup-text">
                         <p>¿Aún no tienes una cuenta?</p>
-                        <Link href='/SignUp'>Regístrate aquí!</Link>
+                        <Link to="/SignUp" className="signup-link">Regístrate aquí!</Link>
                     </div>
                 </div>
-            </form >
-
-        </div >
-
+            </form>
+        </div>
     );
 }
+
 export default LogIn;
-
-
