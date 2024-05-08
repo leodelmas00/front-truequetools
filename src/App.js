167fdda05f70ee92036d4d3973f951bfa371b86e
Lisandro Piva <lisandropiva@gmail.com>
2024-05-08 16:36:06 -0300

    volver

--- src/App.js
+++ src/App.js
@@ -26,6 +26,10 @@ function App() {
       <Route path="/Form" component={Form}/>
       <Route path="/Post" component={Post}/>
       <Route path="/AdminView" component={AdminView}/>
+      <Route path="/PostList" component={PostList}/>
+      <Route path="/UserList" component={UserList}/>
+      <Route path="/CommentList" component={CommentList}/>
+      <Route path="/PostDetail" component={PostDetail}/>
     </Switch>
   );
 }
