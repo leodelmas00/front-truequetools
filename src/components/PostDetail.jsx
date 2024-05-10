2ad257ea28aa9ce127df84965e15a3be481ba53e
Lisandro Piva <lisandropiva@gmail.com>
2024-05-10 01:56:44 -0300

    cambios en el postdetail visuales mas que nada

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -2,63 +2,53 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { baseURL } from '../api/trueque.api';
 import { useParams } from "wouter";
-import '../styles/PostDetailStyle.css'
-
+import { Link } from 'wouter';
+import '../styles/PostDetailStyle.css';
 
 function PostDetail() {
     const [post, setPost] = useState(null);
-    const [nuevoComentario, setNuevoComentario] = useState(''); // Estado para el nuevo comentario
-    const params = useParams(); // Obtener el postId de los parámetros de la URL
-    const [loading, setLoading] = useState(true)
+    const [nuevoComentario, setNuevoComentario] = useState('');
+    const params = useParams();
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const fetchPost = async () => {
             try {
-                console.log(params.postId);
-                const token = localStorage.getItem('token'); // Obtener el token JWT del almacenamiento local
-                console.log(token)
+                const token = localStorage.getItem('token');
                 const response = await axios.get(`${baseURL}post/${params.postId}/`, {
                     headers: {
                         Authorization: `Token ${token}`,
                     }
                 });
                 setPost(response.data);
-                console.log(response.data)
             } catch (error) {
                 console.error('Error:', error);
             }
             finally {
-                setLoading(false)
+                setLoading(false);
             }
         };
         if (loading) {
             fetchPost();
         }
-    }, [post, loading]);
+    }, [params.postId, loading]);
 
-
-    if (loading) {
-        return <div>Cargando...</div>;
-    }
-
-    // Manejador de cambios para el input del comentario
     const handleInputChange = (event) => {
         setNuevoComentario(event.target.value);
     };
 
-    // Manejador de envío del formulario de comentario
     const handleSubmit = async (event) => {
         event.preventDefault();
         try {
             const token = localStorage.getItem('token');
             const response = await axios.post(`${baseURL}post/${params.postId}/comments/`, {
+                contenido: nuevoComentario // Agregar el contenido del comentario
+            }, {
                 headers: {
                     Authorization: `Token ${token}`,
                 }
             });
-            console.log(response.status);
             if (response.status === 201 && response.data) {
-                console.log("COMENTARIO ENVIADO");
                 setPost(prevPost => ({
                     ...prevPost,
                     comentarios: [...prevPost.comentarios, response.data]
@@ -66,38 +56,51 @@ function PostDetail() {
                 setNuevoComentario('');
             }
         } catch (error) {
-            console.log(error);
+            console.error(error);
         }
     };
 
+    if (loading) {
+        return <div>Cargando...</div>;
+    }
+
     return (
-        <div className="post-container">
-            <div className="post-card">
-                <p className="post-date">{post.fecha}</p>
-                <h3>Subido por: {post.usuario_propietario.username}</h3>
-                <h1 className="post-title">{post.titulo}</h1>
-                <p className="post-description">{post.descripcion}</p>
-            </div>
-            <div className="post-comments">
-                <h2>Comentarios:</h2>
-                {post.comentarios.map((comentario, index) => (
-                    <div key={index} className="comment">
-                        <p>{comentario.contenido}</p>
-                        <p>Por: {comentario.usuario_propietario.username}</p>
+            <div className="background-image-published">
+                <div className="post-container">
+                    <div className="post-card">
+                        <p className="post-date">{post.fecha}</p>
+                        <hr></hr>
+                        <h3>Subido por: {post.usuario_propietario.username}</h3>
+                        <h1 className="post-title">{post.titulo}</h1>
+                        <p className="post-description">{post.descripcion}</p>
                     </div>
-                ))}
-                <form onSubmit={handleSubmit}>
-                    <input
-                        type="text"
-                        value={nuevoComentario}
-                        onChange={handleInputChange}
-                        placeholder="Escribe un comentario..."
-                        className="comment-input"
-                    />
-                    <button type="submit" className="comment-button">Enviar</button>
-                </form>
+                    <div className="post-comments">
+                        <h2>Comentarios:</h2>
+                        {post.comentarios.map((comentario, index) => (
+                            <div key={index} className="comment">
+                                <div className='comment-letter'>{comentario.contenido}</div>
+                                <p className='comment-letter'>Por: {comentario.usuario_propietario.username}</p>
+                            </div>
+                        ))}
+                        <form onSubmit={handleSubmit}>
+                            <input
+                                type="text"
+                                value={nuevoComentario}
+                                onChange={handleInputChange}
+                                placeholder="Escribe un comentario..."
+                                className="input-field-comment"
+                                
+                                maxLength={200}
+                            />
+                            <button type="submit" className="comment-button">Enviar</button>
+                        </form>
+                    </div>
+                </div>
+                
+                
+                    <Link to="/SignIn" className={"signin-link-from-postdetail"}>Volver al inicio</Link>
+                
             </div>
-        </div>
     );
 }
 
