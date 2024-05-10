9c4fc98f98c3ed931ffb77992201fe38c18ce12f
Lisandro Piva <lisandropiva@gmail.com>
2024-05-10 02:27:15 -0300

    Mas cambios visuales

--- src/components/PostDetail.jsx
+++ src/components/PostDetail.jsx
@@ -72,27 +72,33 @@ function PostDetail() {
                         <hr></hr>
                         <h3>Subido por: {post.usuario_propietario.username}</h3>
                         <h1 className="post-title">{post.titulo}</h1>
+                        
                         <p className="post-description">{post.descripcion}</p>
                     </div>
                     <div className="post-comments">
                         <h2>Comentarios:</h2>
                         {post.comentarios.map((comentario, index) => (
                             <div key={index} className="comment">
+                                <p className='comment-letter'><b>Por:</b> {comentario.usuario_propietario.username}</p>
+                                <hr className='margenhr'></hr>
                                 <div className='comment-letter'>{comentario.contenido}</div>
-                                <p className='comment-letter'>Por: {comentario.usuario_propietario.username}</p>
+                                
+                                
                             </div>
                         ))}
                         <form onSubmit={handleSubmit}>
+                        <div>
                             <input
                                 type="text"
                                 value={nuevoComentario}
                                 onChange={handleInputChange}
                                 placeholder="Escribe un comentario..."
                                 className="input-field-comment"
-                                
                                 maxLength={200}
                             />
-                            <button type="submit" className="comment-button">Enviar</button>
+                        </div>
+                        <button type="submit" className="comment-button">Enviar</button>
+                        <p>Caracteres ingresados: {nuevoComentario.length}/200</p>
                         </form>
                     </div>
                 </div>
