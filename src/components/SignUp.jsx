298e64c9f92a6333f3894f03d04b5b81e96cf3bf
GeronimoErrante <geronimoerrante7@gmail.com>
2024-05-14 22:25:39 -0300

    arreglo bugs y agrego restriccion de edad

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -28,6 +28,10 @@ function SignUp() {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+        if (form.password.length < 6) {
+            setError('La contraseña debe tener al menos 6 caracteres');
+            return;
+        }
         try {
             const response = await axios.post(baseURL + 'register/', form);
             console.log(response.status);
@@ -63,45 +67,45 @@ function SignUp() {
     return (
         <div className="container background-img">
             <div className='container-signup'>
-                    <div className='cajaFormulario'>
-                        <header className="titulo"> Trueque<span style={{ color: '#BF4C41' }}>Tools</span> </header>
-                        <div> <p className="subtitulo"> Registrarse, ¡más fácil que nunca! </p> </div>
-                        <form onSubmit={handleSubmit}>
-                            <div>
-                                <div>Nombre de usuario</div>
-                                <input className="input-user" name="username" placeholder="Ingrese su nombre de usuario" required onChange={handleChange} />
-                            </div>
-                            <div>
-                                <div>Correo electrónico</div>
-                                <input className="input-user" placeholder="Ingrese su correo" name="email" type="email" required onChange={handleChange} />
-                            </div>
-                            <div>
-                                <div>Contraseña</div>
-                                <input className="input-user" placeholder="Ingrese su contraseña" type="password" name="password" required onChange={handleChange} />
-                            </div>
-                            <div>
-                                <div>Fecha de nacimiento</div>
-                                <input className='input-user' type="date" name="fecha_de_nacimiento" placeholder="Dia" required onChange={handleChange} />
-                            </div>
+                <div className='cajaFormulario'>
+                    <header className="titulo"> Trueque<span style={{ color: '#BF4C41' }}>Tools</span> </header>
+                    <div> <p className="subtitulo"> Registrarse, ¡más fácil que nunca! </p> </div>
+                    <form onSubmit={handleSubmit}>
+                        <div>
+                            <div>Nombre de usuario</div>
+                            <input className="input-user" name="username" placeholder="Ingrese su nombre de usuario" required onChange={handleChange} />
+                        </div>
+                        <div>
+                            <div>Correo electrónico</div>
+                            <input className="input-user" placeholder="Ingrese su correo" name="email" type="email" required onChange={handleChange} />
+                        </div>
+                        <div>
+                            <div>Contraseña</div>
+                            <input className="input-user" placeholder="Ingrese su contraseña" type="password" name="password" required onChange={handleChange} />
+                        </div>
+                        <div>
+                            <div>Fecha de nacimiento</div>
+                            <input className='input-user' type="date" name="fecha_de_nacimiento" placeholder="Dia" required onChange={handleChange} />
+                        </div>
 
-                            <div className="input-container">Sucursal favorita</div>
-                            <select
-                                name="sucursal_favorita"
-                                value={form.sucursal_favorita}
-                                onChange={handleChange}
-                                className="input-select-sucursal"
-                                required
-                                style={{ fontSize: '20px' }}
-                            >
-                                <option className="input-select-sucursal" value="" disabled>Selecciona una sucursal</option>
-                                {sucursales.map((sucursal, index) => (
-                                    <option key={sucursal.id} value={sucursal.id}>{sucursal.nombre}{sucursal.direccion}</option>
-                                ))}
-                            </select>
-                            {error && <h5 className="error-message">{error}</h5>}
-                            <div className="botonRegistrar"> <button>Registrarse</button></div>
-                        </form>
-                    </div>
+                        <div className="input-container">Sucursal favorita</div>
+                        <select
+                            name="sucursal_favorita"
+                            value={form.sucursal_favorita}
+                            onChange={handleChange}
+                            className="input-select-sucursal"
+                            required
+                            style={{ fontSize: '20px' }}
+                        >
+                            <option className="input-select-sucursal" value="" disabled>Selecciona una sucursal</option>
+                            {sucursales.map((sucursal, index) => (
+                                <option key={sucursal.id} value={sucursal.id}>{sucursal.nombre}{sucursal.direccion}</option>
+                            ))}
+                        </select>
+                        {error && <h5 className="error-message">{error}</h5>}
+                        <div className="botonRegistrar"> <button>Registrarse</button></div>
+                    </form>
+                </div>
             </div>
         </div>
     );
