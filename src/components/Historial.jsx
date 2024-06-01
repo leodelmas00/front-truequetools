6201bfaf1d50add3556369bc82d19bbddeeaf8ae
Leonardo Delmas <delmasleonardo@gmail.com>
2024-06-01 00:33:52 -0300

    Update Historial
    - Ahora solo aparecen los titulos de los post
    - Se habilito el boton de cancelar, cuando se presiona se cancela el intercambio si se esta a mas de 1 dia de la fecha del intercambio

--- src/components/Historial.jsx
+++ src/components/Historial.jsx
@@ -5,10 +5,14 @@ import { baseURL } from '../api/trueque.api';
 import axios from 'axios';
 import { formatFechaSolicitud } from '../utils';
 import PostDetailHistory from './PostDetailHistory';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
 
 function Historial() {
     const [solicitudesConSucursal, setSolicitudesConSucursal] = useState([]);
     const [error, setError] = useState(null);
+    const [openSuccess, setOpenSuccess] = useState(false);
+    const [openError, setOpenError] = useState(false);
 
     useEffect(() => {
         const fetchHistorial = async () => {
@@ -41,6 +45,38 @@ function Historial() {
         });
     };
 
+    const handleDelete = async (postid) => {
+        try {
+            const token = localStorage.getItem('token');
+            const response = await axios.delete(`${baseURL}solicitudes/${postid}/cancel`, {
+                headers: {
+                    Authorization: `Token ${token}`,
+                }
+            });
+            console.log(response.status);
+            if (response.status === 204) {
+                setOpenSuccess(true);
+            }
+        } catch (error) {
+            console.log(error);
+            if (error.response && (error.response.status === 409 || error.response.status === 400)) {
+                setError('No se puede cancelar un trueque con menos de un dia de anticipacion');
+                setOpenError(true);
+            } else {
+                console.error('Error:', error);
+                setError('Ha ocurrido un error. Por favor, inténtelo nuevamente.');
+            }
+        }
+    };
+
+    const handleSuccessClose = () => {
+        setOpenSuccess(false);
+    };
+
+    const handleCloseError = () => {
+        setOpenError(false);
+    };
+
     return (
         <div className='Historial'>
             <h1 className='Titulo'> HISTORIAL </h1>
@@ -65,9 +101,22 @@ function Historial() {
                                 <td>
                                     <PostDetailHistory postId={solicitud.publicacion_deseada} onSucursalLoaded={(sucursal) => handleSucursalLoaded(sucursal, solicitud.publicacion_deseada)} />
                                 </td>
-                                <td>{formatFechaSolicitud(solicitud.fecha)}</td>
+                                <td>{formatFechaSolicitud(solicitud.fecha_del_intercambio)}</td>
                                 <td>{solicitud.sucursal ? `${solicitud.sucursal.nombre} - ${solicitud.sucursal.direccion}` : ''}</td>
                                 <td>{solicitud.estado}</td>
+                                {solicitud.estado === "PENDIENTE" && (
+                                    <button className='boton-cancelar' onClick={() => handleDelete(solicitud.id)}>Cancelar</button>
+                                )}
+                                <Snackbar open={openError} autoHideDuration={6000} onClose={handleCloseError}>
+                                    <MuiAlert elevation={6} variant="filled" onClose={handleCloseError} severity="error">
+                                        {error}
+                                    </MuiAlert>
+                                </Snackbar>
+                                <Snackbar open={openSuccess} autoHideDuration={6000} onClose={handleSuccessClose}>
+                                    <MuiAlert elevation={6} variant="filled" onClose={handleSuccessClose} severity="success">
+                                        Se cancelo con exito!
+                                    </MuiAlert>
+                                </Snackbar>
                             </tr>
                         ))}
                     </tbody>
